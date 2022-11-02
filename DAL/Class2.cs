ebf4ba252be385e240c4ea49d8d4559f2b6d2303
saivignesh2001s <saivignesh2001s@gmail.com>
2022-11-02 18:18:13 +0530

    gd

---
+++ DAL/Class2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class CustInfoLogMethods
+    {
+        simplilearnprojectEntities context = null;
+        public CustInfoLogMethods()
+        {
+            context = new simplilearnprojectEntities();
+        }
+        public List<CustLogInfo> GetCustLogInfos()
+        {
+            return context.CustLogInfoes.ToList();
+        }
+        public bool SaveCustLog(CustLogInfo t)
+        {
+            try
+            {
+                context.CustLogInfoes.Add(t);
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+    public class UserInfomethods
+    {
+        simplilearnprojectEntities user = null;
+        public UserInfomethods()
+        {
+            user = new simplilearnprojectEntities();
+        }
+        public UserInfo Checklogin(int kp,string p)
+        {
+            UserInfo k = null;
+            foreach(var item in user.UserInfoes.ToList())
+            {
+                if( kp== item.UserId && p== item.Password_)
+                {
+                    k = item;
+                }
+            }
+            return k;
+        }
+    }
+}
---
+++ DALTest/Class1.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace DALTest
+{
+    [TestFixture]
+    public class Custinfologtest
+    {
+        simplilearnprojectEntities context = new simplilearnprojectEntities();
+       
+        [TestCase]
+        public void GetallloginfoesTest()
+        {
+            bool k = true;
+            List<CustLogInfo> m= context.CustLogInfoes.ToList();
+            if (m != null)
+            {
+                k = true;
+            }
+            else
+            {
+                k = false;
+
+            }
+            Assert.IsTrue(k);
+        }
+        [TestCase]
+        public void SaveCustLogInfoTest()
+        {
+            bool k = true;
+            try
+            {
+                
+                context.SaveChanges();
+                k=true;
+
+            }
+            catch
+            {
+                k=false;
+            }
+            Assert.IsTrue(k);
+        }
+
+    }
+    [TestFixture]
+    public class Userinfologtest
+    {
+        simplilearnprojectEntities context = null;
+        public Userinfologtest()
+        {
+            context = new simplilearnprojectEntities();
+        }
+        [TestCase(2,"stuart@123",ExpectedResult =true)]
+        public bool Logintest(object ans1,object ans2)
+        {
+            bool k = false;
+            foreach(var item in context.UserInfoes.ToList())
+            {
+                if(item.UserId==(int)ans1 && item.Password_ == ans2.ToString())
+                {
+                    k = true;
+                }
+            }
+            return k;
+        }
+    }
+}
